578736b312d803c4865afecd6b2401386f1d9359
luochao <1055120207@qq.com>
2024-01-09 11:09:09 +0800

    perf: perf load cdn

--- src/components/app-title/index.tsx
+++ src/components/app-title/index.tsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Env } from "../../config";
 import { getConfig } from "../../core/http/config";
 
 export function ModifyAppTitle() {
+  const isFlagRef = useRef(true);
+
   useEffect(() => {
-    init();
+    if (isFlagRef.current) {
+      initAppTitle();
+      initAppCDN();
+      isFlagRef.current = false;
+    }
   }, []);
 
-  function init() {
+  function initAppTitle() {
     const config = getConfig();
     const element = document.getElementsByTagName("title") || [];
 
     if (config.env === Env.zh && element[0]) {
       element[0].textContent = config.appNameZH;
     }
+  }
+
+  function initAppCDN() {
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/api-spec-converter@2.12.0/dist/api-spec-converter.js";
+    document.body.appendChild(script);
   }
 
   return null;
