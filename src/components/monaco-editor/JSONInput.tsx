0147160b54a3c4216a0d210877bb77b5a38b95df
luochao <1055120207@qq.com>
2024-01-22 23:57:49 +0800

    perf: perf monaco-editor languages file

--- src/components/monaco-editor/JSONInput.tsx
+++ src/components/monaco-editor/JSONInput.tsx
@@ -1,7 +1,9 @@
 import { Editor, loader } from "@monaco-editor/react";
-import * as monaco from "monaco-editor";
+import "monaco-editor/esm/vs/editor/editor.all.js";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import "monaco-editor/esm/vs/language/json/monaco.contribution";
 import { ISchema } from "../../openapi/type";
 
 self.MonacoEnvironment = {
