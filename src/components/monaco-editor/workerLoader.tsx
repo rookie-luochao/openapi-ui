61b422b5c0b8583c30415f5716c08915b9dd2197
luochao <1055120207@qq.com>
2024-05-25 14:59:33 +0800

    perf: perf monaco loader

--- src/components/monaco-editor/workerLoader.tsx
+++ src/components/monaco-editor/workerLoader.tsx
@@ -2,17 +2,20 @@ import { loader } from "@monaco-editor/react";
 // import "monaco-editor/esm/vs/editor/editor.all.js";
 import "monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController.js";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
-import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker&inline";
+// import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker&inline";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker&inline";
 import "monaco-editor/esm/vs/language/json/monaco.contribution";
 
 self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === "json") {
-      return new jsonWorker();
-    }
+  // getWorker(_, label) {
+  //   if (label === "json") {
+  //     return new jsonWorker();
+  //   }
 
-    return new editorWorker();
+  //   return new editorWorker();
+  // },
+  getWorker() {
+    return new jsonWorker();
   },
 };
 
