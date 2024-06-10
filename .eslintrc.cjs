70ea01d121cc977f08ef013049e87b37e522be70
luochao <1055120207@qq.com>
2024-06-10 23:04:26 +0800

    feat: add eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
   ignorePatterns: ["node_modules", "dist", "demo", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react"],
+  plugins: ["react-refresh", "react", "import"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "@typescript-eslint/no-explicit-any": ["off"],
@@ -16,8 +16,20 @@ module.exports = {
         ignoreCase: true,
         noSortAlphabetically: false, // when true, alphabetical order is not enforced
         reservedFirst: true, // react reserved props (children, dangerouslySetInnerHTML, key...) must be listed before all other props
-        multiline: "last", // 'ignore' | 'first' | 'last'
+        multiline: "last", // "ignore" | "first" | "last"
       },
     ],
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+    },
   },
 };
