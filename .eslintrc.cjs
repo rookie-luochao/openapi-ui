e4fc93e53c3279ea0ae38ca96321bf054db23830
luochao <1055120207@qq.com>
2024-09-27 14:14:28 +0800

    fix: fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,7 +28,7 @@ module.exports = {
     "import/resolver": {
       typescript: {
         alwaysTryTypes: true,
-        project: "./tsconfig.json",
+        project: "./tsconfig.app.json",
       },
     },
   },
